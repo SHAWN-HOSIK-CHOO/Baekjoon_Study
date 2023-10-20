5b8295a08e20e20b48d386824a076829bd974044
SHAWN HOSIK CHOO <hosikchoo1@gmail.com>
2023-10-20 21:03:18 +0900

    nothing

--- bj1707.cpp
+++ bj1707.cpp
@@ -1,45 +1,88 @@
+/*
+    도저히 안되겠어서 코드 참고했습니다
+    https://m42-orion.tistory.com/61
+    감사합니다 
+*/
 #include <iostream>
-#include <stack>
 #include <vector>
+#include <set>
+#include <stack>
 
-void DFS(std::vector<std::vector<int>>& g, std::vector<int>& c, int start)
+void Coloring_DFS(std::vector<std::vector<int>>& graph,std::vector<int>& color_vector,int start)
 {
+    if(!color_vector[start])
+    {
+        color_vector[start] = 1;
+    }
 
+    for(int i = 0; i < graph[start].size(); i++)
+    {
+        int next = graph[start][i];
+
+        if(!color_vector[next])
+        {
+            if(color_vector[start] == 1) color_vector[next] = 2;
+            else if(color_vector[start] == 2) color_vector[next] = 1;
+            Coloring_DFS(graph,color_vector,next);
+        }
+    }
+    
+}
+
+bool isBipartiteGraph(std::vector<std::vector<int>>& graph, std::vector<int>& color_vector, int V)
+{
+    for(int i = 1; i < V; i++)
+    {
+        for(int j = 0; j < graph[i].size(); j++)
+        {
+            if(color_vector[i] == color_vector[graph[i][j]])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 int main()
 {
-    int K, V, E, temp1, temp2;
+    int K, V, E;
     std::vector<std::vector<int>> graph;
-    std::vector<int> offset;
-    // color == 0 means unvisited vertex
-    std::vector<int> color;
-
-    offset.push_back(1);
-    // At index 0 -> not using it
-    graph.push_back(std::vector<int>());
+    std::vector<int> color_vector;
 
     std::cin>>K;
 
-    for(int i = 1; i <= K; i++)
+    while (K--)
     {
         std::cin>>V>>E;
-        offset.push_back(V + offset[i - 1]);
+        
+        graph.assign(V+1, std::vector<int>(0,0));
+        color_vector.assign(V + 1, 0);
 
-        for(int j = 0; j < V; j++ )
+        for(int i = 0; i < E; i++)
         {
-            graph.push_back(std::vector<int>());
-        }
-
-        for(int k = 0; k < E; k++)
-        {   
+            int temp1, temp2;
             std::cin>>temp1>>temp2;
             graph[temp1].push_back(temp2);
             graph[temp2].push_back(temp1);
         }
-    }
 
-    
-    
+        for(int i = 1; i <= V; i++)
+        {
+            if(!color_vector[i])
+            {
+                Coloring_DFS(graph,color_vector,i);
+            }
+        }
+
+        if(isBipartiteGraph(graph,color_vector,V))
+        {
+            std::cout<<"YES"<<'\n';
+        }
+        else
+        {
+            std::cout<<"NO"<<'\n';
+        }
+    }
     return 0;
 }
--- dummy.cpp
+++ dummy.cpp
@@ -1,41 +1,164 @@
 #include <iostream>
 #include <vector>
-using namespace std;
-bool ans = false;
+#include <set>
+#include <stack>
 
-void dfs(int k,int cur, vector<vector<int>> &v,vector<bool> &vis) {
-	if (k == 4) {
-		ans = true;
-		return;
-	}
-	for (int j = 0; j < v[cur].size(); j++) {
-		int nxt = v[cur][j];
-		if (vis[nxt]) continue;
-		vis[nxt] = 1;
-		dfs(k + 1, nxt, v, vis);
-		vis[nxt] = 0;
-	}
+enum class Color
+{
+    NONE,RED,BLACK
+};
+
+void Coloring_DFS(std::vector<std::vector<int>>& graph, std::set<int>& visited, std::vector<Color>& color, int start)
+{
+    std::stack<int> stack;
+    stack.push(start);
+
+    Color cur_color = Color::RED;
+    color[start] = cur_color;
+
+    while (!stack.empty())
+    {
+        int idx = stack.top();
+        stack.pop();
+       
+        if(visited.find(idx) == visited.end())
+        {
+            visited.insert(idx);
+
+            if(cur_color == Color::RED)
+            {
+                cur_color = Color::BLACK;
+            }
+            else if(cur_color == Color::BLACK)
+            {
+                cur_color = Color::RED;
+            }            
+
+            for(int i = 0; i < graph[idx].size(); i++)
+            {
+                if(visited.find(graph[idx][i]) == visited.end())
+                {
+                    stack.push(graph[idx][i]);
+
+                    color[graph[idx][i]] = cur_color;                    
+                }
+            }
+        }
+    }
+    
 }
 
-int main() {
-	int n, m;
-	cin >> n >> m;
+// void Coloring_DFS(std::vector<std::vector<int>>& graph, std::set<int>& visited, std::vector<Color>& color, int start)
+// {
+//     if(visited.find(start) == visited.end())
+//     {
+//         visited.insert(start);
+//         color[start] = Color::RED;
+//     }
 
-	vector<vector<int>> v(n);
+//     for(int i = 0; i < graph[start].size(); i++)
+//     {
+//         int next = graph[start][i];
 
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-	}
-	for (int i = 0; i < n; i++) {
-		vector<bool> vis(n);
-		vis[i] = 1;
-		dfs(0, i, v, vis);
-		if (ans) break;
-	}
-	cout << ans;
+//         if(visited.find(next) == visited.end())
+//         {
+//             if(color[start] == Color::RED) color[next] = Color::BLACK;
+//             else if(color[start] == Color::BLACK) color[next] = Color::RED;
+//             Coloring_DFS(graph,visited,color,next);
+//         }
+//     }
+// }
 
-	return 0;
+bool isBipartiteGraph(std::vector<std::vector<int>>& graph, std::vector<Color>& color)
+{
+    for(int i = 1; i <= graph.size(); i++)
+    {
+        for(int j = 0; j < graph[i].size(); j++)
+        {
+            if(color[i] == color[graph[i][j]])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int K, V, E;
+    std::vector<std::vector<int>> graph;
+    std::set<int> visited_set;
+    std::vector<Color> color_vector;
+
+    std::cin>>K;
+
+    while (K--)
+    {
+        std::cin>>V>>E;
+
+        color_vector.resize(V+1,Color::NONE);
+
+        for(int i = 0; i <= V; i++)
+        {
+            graph.push_back(std::vector<int>());
+        }
+
+        for(int i = 0; i < E; i++)
+        {
+            int temp1, temp2;
+            std::cin>>temp1>>temp2;
+            graph[temp1].push_back(temp2);
+            graph[temp2].push_back(temp1);
+        }
+
+        for(int i = 1; i <= V; i++)
+        {
+            if(visited_set.find(i) == visited_set.end())
+            {
+                Coloring_DFS(graph,visited_set,color_vector,i);
+            }
+        }
+
+        if(isBipartiteGraph(graph,color_vector))
+        {
+            std::cout<<"YES"<<'\n';
+        }
+        else
+        {
+            std::cout<<"NO"<<'\n';
+        }
+
+
+        //Debug
+        /*
+        for(auto v : color_vector)
+        {
+            if(v == Color::RED)
+            {
+                std::cout<<"RED"<<" ";
+            }
+            else if(v == Color::BLACK)
+            {
+                std::cout<<"BLACK"<<" ";
+            }
+            else
+            {
+                std::cout<<"NONE"<<" ";
+            }
+        }
+
+        */
+        // Clearing everything for the next loop
+        // Clear Graph
+        for(int i = 0; i <= V; i++)
+        {
+            graph[i].clear();
+        }
+        graph.clear();
+
+        //Clear visited set
+        visited_set.clear();
+    }
+    return 0;
 }
