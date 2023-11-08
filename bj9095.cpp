4a630b3febfa97a13ed00929731c9d97bcaf72c2
SHAWN HOSIK CHOO <hosikchoo1@gmail.com>
2023-11-08 23:23:54 +0900

    08/11/2023

---
+++ bj9095.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int T, n;
+    std::vector<int> v;
+
+    std::cin>>T;
+
+    for(int j = 0; j < T; j++){
+
+        std::cin>>n;
+
+        v.assign(n+1, 0);
+        v[1] = 1;
+        v[2] = 2;
+        v[3] = 4;
+
+        for(int i = 4; i <= n; i++)
+        {
+            v[i] = v[i - 1] + v[i - 2] + v[i - 3];
+        }
+
+        std::cout<<v[n]<<'\n';
+    }
+
+    return 0;
+}
